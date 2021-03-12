f682f39b3bc260a269671b4b2cc7a1a9a9e50bb6
Thatcher Peskens <thatcher@leaf.cloud>
2021-03-12 11:49:42 +0100

    A cleaner routing

---
+++ src/routes/[...slug].svelte
@@ -0,0 +1,14 @@
+<script context="module">
+     export async function preload({ params, query }) {
+    
+          return { slug: params.slug }
+  }
+</script>
+<script>
+    export let slug
+</script>
+
+
+SLUG HERE
+
+{ slug }
